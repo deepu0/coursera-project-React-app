2f5bb496b0377171a6e4fe713426b946f2382544
DEEPAK KUMAR <sdeepaksharma834@gmail.com>
2021-02-12 13:14:24 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Navbar, NavbarBrand } from 'reactstrap';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+          </div>
+        </Navbar>
+      </div>
+    );
+  }
+}
+
+export default App;
